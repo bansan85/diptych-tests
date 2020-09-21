a78d42eca97a0e1eb01c940543450c0b97d7d262
LE GARREC Vincent <github@le-garrec.fr>
2020-09-21 07:44:05 +0200

    Add an image where 2 pages comes 2 contours

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,8 @@ sys.path.append("..")
 
 import script
 
+script.DEBUG = False
+
 from multiprocessing import Process
 
 
@@ -21,12 +23,11 @@ run_cpu_tasks_in_parallel(
         lambda: script.SeparatePage().treat_file(
             "0001.png",
             {
+                "separation contour double": (0, "ecart", 0),
                 "separation double page angle": (90.7, "ecart", 0.2),
                 "separation double page y=0": (2486, "pourcent", 0.002),
                 "page rotation 1": (0.8, "ecart", 0.2),
                 "page rotation 2": (0.0, "ecart", 0.2),
-                "image 1 dpi": (300, "ecart", 0.0000001),
-                "image 2 dpi": (300, "ecart", 0.0000001),
                 "image 1 crop x1": (331, "pourcent", 0.01),
                 "image 1 crop y1": (337, "pourcent", 0.01),
                 "image 1 crop x2": (2343, "pourcent", 0.002),
@@ -35,7 +36,29 @@ run_cpu_tasks_in_parallel(
                 "image 2 crop y1": (648, "pourcent", 0.01),
                 "image 2 crop x2": (2181, "pourcent", 0.002),
                 "image 2 crop y2": (3362, "pourcent", 0.002),
+                "image 1 dpi": (300, "ecart", 0.0000001),
+                "image 2 dpi": (300, "ecart", 0.0000001),
             },
-        )
+        ),
+        lambda: script.SeparatePage().treat_file(
+            "2-pages-2-contours.png",
+            {
+                "separation contour double": (1, "ecart", 0),
+                "separation double page angle": (90.3, "ecart", 0.2),
+                "separation double page y=0": (2494, "pourcent", 0.002),
+                "page rotation 1": (0.0, "ecart", 0.2),
+                "page rotation 2": (0.0, "ecart", 0.2),
+                "image 1 crop x1": (2474, "pourcent", 0.01),
+                "image 1 crop y1": (3466, "pourcent", 0.01),
+                "image 1 crop x2": (98, "pourcent", 0.002),
+                "image 1 crop y2": (23, "pourcent", 0.002),
+                "image 2 crop x1": (96, "pourcent", 0.01),
+                "image 2 crop y1": (241, "pourcent", 0.01),
+                "image 2 crop x2": (2148, "pourcent", 0.002),
+                "image 2 crop y2": (3242, "pourcent", 0.002),
+                "image 1 dpi": (300, "ecart", 0.0000001),
+                "image 2 dpi": (300, "ecart", 0.0000001),
+            },
+        ),
     ]
 )
